e1adac5864761091b7390182a6f8ac226f13df2d
Diego Camacho <zenpaicodes@gmail.com>
2022-12-07 09:41:55 -0500

    add readline-sync packague

---
+++ FullStack-BootCamp/MODULE9/M9-projects/Node-calculator/nodeCalc.js
@@ -0,0 +1,11 @@
+// Ask the user, "What operation would you like to perform?"
+
+var readL = require("Which operation would you like to perform? ");
+
+
+// Then the user enters one of these options: "/" "*" "-" "+"
+// If the user enters an invalid character, print: "That is not a valid operation" and then restart the program
+// After the user enters a valid operation, ask the user, "Please enter the first number"
+// The user then enters the first number. If the user enters something that is not a number, print: “This is not a number” and then re-ask the question
+// After a valid number is entered, ask the user, "Please enter the second number". Perform the same error handling as before
+// Then create a function to perform the proper math operation and print the result as: "The result is: X" where "X" is the actual result
